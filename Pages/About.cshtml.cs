8e7cd75c159875e3162f39a3e6c6c793fe314f7f
Przemek <przemek.lubczanski@outlook.com>
2024-09-19 23:50:30 +0200

    Editing home i contact page

--- Pages/About.cshtml.cs
+++ Pages/About.cshtml.cs
@@ -12,7 +12,7 @@ namespace myFirstAzureWebApp.Pages
 
         public void OnGet()
         {
-            Message = "Your application description page.";
+            Message = "Test Przemek";
         }
     }
 }
